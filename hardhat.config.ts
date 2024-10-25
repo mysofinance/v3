42ecc4d7b635e2229ce6382ce743c813f11a0c69
asardon <as@myso.finance>
2024-10-25 18:04:01 +0200

    add oracle tests and make stale time be deployment parameter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -28,6 +28,12 @@ const config: HardhatUserConfig = {
       },
       chainId: 42161,
       */
+      /*
+      forking: {
+        url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_MAINNET_API_KEY}`,
+      },
+      chainId: 1,
+      */
     },
   },
   etherscan: {
