f3564be63857d81b40c38072508f08e83db4a98b
Paweł Kędzia <67273035+KedziaPawel@users.noreply.github.com>
2026-03-26 10:44:41 +0100

    chore: rename envs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -18,13 +18,18 @@ const config: HardhatUserConfig = {
   networks: {
     sepolia: {
       chainId: 11155111,
-      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_SEPOLIA_API_KEY}`,
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [`${process.env.SEPOLIA_DEPLOYER_KEY}`],
     },
     baseSepolia: {
       chainId: 84532,
-      url: `https://base-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_SEPOLIA_API_KEY}`,
+      url: `https://base-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [`${process.env.SEPOLIA_DEPLOYER_KEY}`],
+    },
+    mainnet: {
+      chainId: 1,
+      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`${process.env.DEPLOYER_KEY}`],
     },
     hardhat: {
       /*
@@ -43,8 +48,8 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
-      sepolia: process.env.SEPOLIA_ETHERSCAN_API_KEY || "",
-      baseSepolia: process.env.BASESEPOLIA_ETHERSCAN_API_KEY || "",
+      sepolia: process.env.ETHERSCAN_API_KEY || "",
+      baseSepolia: process.env.ETHERSCAN_API_KEY || "",
     },
   },
 };
